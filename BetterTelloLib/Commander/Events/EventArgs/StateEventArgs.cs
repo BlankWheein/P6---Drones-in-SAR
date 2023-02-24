177800f4bfe809b2d8c2deed0641c004c860443a
Blank <47646799+BlankWheein@users.noreply.github.com>
2023-02-24 12:56:38 +0100

    Refactor + Events + video stream

---
+++ BetterTelloLib/Commander/Events/EventArgs/StateEventArgs.cs
@@ -0,0 +1,11 @@
+﻿namespace BetterTelloLib.Commander.Events.EventArgs
+{
+    public class StateEventArgs : System.EventArgs
+    {
+        public TelloState State;
+        public StateEventArgs(TelloState state)
+        {
+            State = state;
+        }
+    }
+}
