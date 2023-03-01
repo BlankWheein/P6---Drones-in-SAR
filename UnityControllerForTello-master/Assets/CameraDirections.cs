1a8d5e011e4186d953731c75acfb9c483fa79d83
Obeline <62429595+Obeline@users.noreply.github.com>
2023-03-01 12:03:48 +0100

    Rotation works fine now
    Rotate with left mouse drag and right mouse drag

--- UnityControllerForTello-master/Assets/CameraDirections.cs
+++ UnityControllerForTello-master/Assets/CameraDirections.cs
@@ -6,40 +6,84 @@ public class CameraDirections : MonoBehaviour
 {
 
     public Transform drone;
+    public Camera DroneCam;
+
     // Start is called before the first frame update
     void Start()
     {
         
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        //transform.LookAt(drone);
-
-
+       
         RotateCamera();
-
         //zoom in/out
-        transform.position += new Vector3(0f, Input.GetAxis("Mouse ScrollWheel")* -100, 0f);
+        if (DroneCam.fieldOfView >= 10 && DroneCam.fieldOfView <= 50)
+        {
+            DroneCam.fieldOfView += Input.GetAxis("Mouse ScrollWheel") * -10;
+        }else if (DroneCam.fieldOfView <= 10)
+        {
+            DroneCam.fieldOfView = 10;
+        }
+        else
+        {
+            DroneCam.fieldOfView = 50;
+        }
+        
+       
     }
 
     void RotateCamera()
     {
+        float xAxis;
+        float yAxis;
 
         if (Input.GetMouseButton(1))
         {
-            float xAxis = Input.GetAxis("Mouse X")*-10;
-            float yAxis = Input.GetAxis("Mouse Y")*10;
-            //float zAxis = Input.GetAxis("Mouse Z") * 10;
 
 
-
+            yAxis = Input.GetAxis("Mouse Y") * 10;
             transform.RotateAround(drone.position, Vector3.right, yAxis);
-            transform.RotateAround(drone.position, Vector3.down, xAxis);
-   
-           // transform.RotateAround(drone.position, Vector3.forward, yAxis);
-        }
 
+
+            //does not work properly
+           /* if (rotValY >= -90 && rotValY <= 0)
+            {
+                // xAxis = Input.GetAxis("Mouse X") * -10;
+                //  transform.RotateAround(drone.position, Vector3.down, xAxis);
+
+                yAxis = Input.GetAxis("Mouse Y") * 10;
+                rotValY += yAxis;
+                transform.RotateAround(drone.position, Vector3.right, yAxis);
+
+            }
+            else if (rotValY < -90)
+            {
+
+                yAxis = Mathf.Abs(Input.GetAxis("Mouse Y") * 10);
+                rotValY += yAxis;
+                transform.RotateAround(drone.position, Vector3.right, yAxis);
+            }
+            else
+            {
+                yAxis = Mathf.Abs(Input.GetAxis("Mouse Y") * 10);
+                rotValY -= yAxis;
+                transform.RotateAround(drone.position, Vector3.right, -yAxis);
+            }*/
+
+
+        }
+        if (Input.GetMouseButton(0))
+        {
+
+            xAxis = Input.GetAxis("Mouse X") * -10;
+            transform.RotateAround(drone.position, Vector3.down, xAxis);
+
+
+
+        }
     }
 }
