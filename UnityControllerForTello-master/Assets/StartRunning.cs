37fe6eb9f8b70766848954115380e799e7fbe025
Mthage20 <90241683+Mthage20@users.noreply.github.com>
2023-03-09 11:49:28 +0100

    Battery percent is now showing
    The battery percent is now showing in the ui.
    
    Also initialBatteryPercent has been added to keep track of the batterypercent that the drone started with

--- UnityControllerForTello-master/Assets/StartRunning.cs
+++ UnityControllerForTello-master/Assets/StartRunning.cs
@@ -7,51 +7,66 @@ using TMPro;
 public class StartRunning : MonoBehaviour
 {
     public TMP_InputField field;
-    public TMP_Text distVal;
+    public TMP_Text batteryVal;
+    public TMP_Text distanceVal;
     public TMP_Text xVal;
     public TMP_Text yVal;
     public TMP_Text zVal;
     public Transform drone;
     private bool onPlay = false;
-    private float xVal0;
-    private float yVal0;
-    private float zVal0;
+    private float xValInitial;
+    private float yValInitial;
+    private float zValInitial;
+
+    public float batteryPercent = 100;
+    private float initialBatteryPercent;
+
     // Start is called before the first frame update
     void Start()
     {
+        //Set the battery percent of the drone when it started 
+        initialBatteryPercent = batteryPercent;
+
         //set the start coordinates of the drone
-        xVal0 = drone.transform.position.x;
-        yVal0 = drone.transform.position.y;
-        zVal0 = drone.transform.position.z;
+        xValInitial = drone.transform.position.x;
+        yValInitial = drone.transform.position.y;
+        zValInitial = drone.transform.position.z;
     }
 
     // Update is called once per frame
     void Update()
     {
-        UpdateDroneCoordinates();
+        batteryVal.SetText(batteryPercent.ToString());
+
+        if (batteryPercent == initialBatteryPercent / 2 || batteryPercent < 20)
+        {
+
+        }
+
+        if (onPlay)
+        {
+            UpdateDroneCoordinates();
+        }
     }
 
     //called when play button is clicked
     public void OnPlay()
-    {   
+    {
         InsertDist();
-        onPlay= true;
+        onPlay = true;
     }
 
     //insert the input distance in the info panel
     private void InsertDist()
     {
-         distVal.text = field.text==""?"0":field.text;
+        distanceVal.text = field.text == "" ? "0" : field.text;
     }
 
     private void UpdateDroneCoordinates()
-    {   
-        if (onPlay)
-        {     
-            //update the infofield witht the coordinates, starting form (0,0,0)
-            xVal.SetText((drone.transform.position.x-xVal0).ToString());
-            yVal.SetText((drone.transform.position.y-yVal0).ToString());
-            zVal.SetText((drone.transform.position.z-zVal0).ToString());
-        }
+    {
+        //update the infofield witht the coordinates, starting form (0,0,0)
+        xVal.SetText((drone.transform.position.x - xValInitial).ToString());
+        yVal.SetText((drone.transform.position.y - yValInitial).ToString());
+        zVal.SetText((drone.transform.position.z - zValInitial).ToString());
     }
 }
