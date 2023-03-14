a9c20b24342b5db85d110ec547b9b898d023b8fc
Obeline <62429595+Obeline@users.noreply.github.com>
2023-03-14 09:52:05 +0100

    Moved scripts to a folder called UI_scripts

--- UnityControllerForTello-master/Assets/Scripts/UI_Scripts/TestMovement.cs
+++ UnityControllerForTello-master/Assets/Scripts/UI_Scripts/TestMovement.cs
@@ -4,11 +4,7 @@ using UnityEngine;
 
 public class TestMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
 
     // Update is called once per frame
     void Update()
--- UnityControllerForTello-master/Assets/Scripts/UI_Scripts/TogglePause.cs
+++ UnityControllerForTello-master/Assets/Scripts/UI_Scripts/TogglePause.cs
@@ -9,17 +9,6 @@ public class TogglePause : MonoBehaviour
     public Image PlayPause;
     public Sprite Play;
     public Sprite Pause;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void ChangeImage()
     {
