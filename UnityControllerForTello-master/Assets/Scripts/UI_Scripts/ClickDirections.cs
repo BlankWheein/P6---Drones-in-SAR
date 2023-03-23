400db51752e3cae05553f3ba1dcdd1c8662b84d8
Obeline <62429595+Obeline@users.noreply.github.com>
2023-03-23 12:57:51 +0100

    Sorta fixed point and click

--- UnityControllerForTello-master/Assets/Scripts/UI_Scripts/ClickDirections.cs
+++ UnityControllerForTello-master/Assets/Scripts/UI_Scripts/ClickDirections.cs
@@ -5,36 +5,39 @@ using UnityEngine.EventSystems;
 
 
 public class ClickDirections : MonoBehaviour, IPointerClickHandler
-    {
+{
 
-    [SerializeField] protected Camera UICamera;
-    [SerializeField] protected RectTransform textureRectTransform;
-    [SerializeField] protected Camera RenderToTextureCamera;
+    [SerializeField] private RectTransform screen;
+    [SerializeField] private Camera droneCam;
     [SerializeField] private GameObject directionObject = null;
+    [SerializeField] private GameObject drone;
     private GameObject oldBlock = null;
     public void OnPointerClick(PointerEventData eventData)
     {
         if (eventData.button == PointerEventData.InputButton.Middle)
         {
-            if(oldBlock != null)
+            if (oldBlock != null)
             {
                 Destroy(oldBlock);
             }
-            Vector2 localPoint;
-            RectTransformUtility.ScreenPointToLocalPointInRectangle(textureRectTransform, eventData.position, UICamera, out localPoint);
-            var rect = textureRectTransform.rect;
-            localPoint.x = (localPoint.x / rect.width) + textureRectTransform.pivot.x;
-            localPoint.y = (localPoint.y / rect.height) + textureRectTransform.pivot.y;
-            Ray ray = RenderToTextureCamera.GetComponent<Camera>().ViewportPointToRay(localPoint);
-            Plane plane = new Plane(Vector3.up, Vector3.zero);
-           
+
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(screen, eventData.position, null, out Vector2 localPoint);
+
+            var rect = screen.rect;
+            localPoint.x = (localPoint.x / rect.width) + screen.pivot.x;
+            localPoint.y = (localPoint.y / rect.height) + screen.pivot.y;
+            Ray ray = droneCam.GetComponent<Camera>().ViewportPointToRay(localPoint);
+            Plane plane = new Plane(Vector2.down, Vector2.left);
+
             plane.Raycast(ray, out float d);
             Vector3 hit = ray.GetPoint(d);
+
+
             GameObject clickPoint = Instantiate(directionObject);
-            clickPoint.transform.position = new Vector3(hit.x, 0, hit.z);
+            clickPoint.transform.position = new Vector3(hit.x, drone.transform.position.y, hit.z);
             clickPoint.name = "Waypoint";
-            oldBlock =clickPoint;
-            Debug.Log("H: " + hit);
+            oldBlock = clickPoint;
+
         }
 
     }
