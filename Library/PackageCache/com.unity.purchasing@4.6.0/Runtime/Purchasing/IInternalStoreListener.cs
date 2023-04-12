18542c46f678b8e47b75904be36545144af6b2b4
Blank <47646799+BlankWheein@users.noreply.github.com>
2023-04-12 10:58:42 +0200

    yolo

---
+++ Library/PackageCache/com.unity.purchasing@4.6.0/Runtime/Purchasing/IInternalStoreListener.cs
@@ -0,0 +1,36 @@
+#nullable enable
+using System;
+
+namespace UnityEngine.Purchasing
+{
+    /// <summary>
+    /// Internal Purchasing interface.
+    /// </summary>
+    internal interface IInternalStoreListener
+    {
+        /// <summary>
+        /// Purchasing failed to initialise for a non recoverable reason.
+        /// </summary>
+        void OnInitializeFailed(InitializationFailureReason error, string? message = null);
+
+        /// <summary>
+        /// A purchase succeeded.
+        /// </summary>
+        PurchaseProcessingResult ProcessPurchase(PurchaseEventArgs e);
+
+        /// <summary>
+        /// A purchase failed with specified reason.
+        /// </summary>
+        void OnPurchaseFailed(Product i, PurchaseFailureReason p);
+
+        /// <summary>
+        /// Purchasing initialized successfully.
+        ///
+        /// The <c>IStoreController</c> is available for accessing
+        /// purchasing functionality.
+        ///
+        /// Initialized products will include receipts, if currently owned.
+        /// </summary>
+        void OnInitialized(IStoreController controller);
+    }
+}
