8b44c5e441378ceaeaec58213b20a8306a731f72
Obeline <62429595+Obeline@users.noreply.github.com>
2023-04-26 12:48:24 +0200

    Added clear button

--- Assets/Scripts/UI_Scripts/TogglePause.cs
+++ Assets/Scripts/UI_Scripts/TogglePause.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using UnityEditorInternal;
+using System.CodeDom;
 
 public class TogglePause : MonoBehaviour
 {
     public Button TakeOff;
     public Button Return;
     public TMP_Dropdown Pattern;
+    public Button Clear;
 
-    BetterTelloManager BetterTelloManager;
+    
     TMP_Text TakeOffText;
     TMP_Text ReturnText;
     TMP_Text PatternText;
+    TMP_Text ClearText;
+
+    BetterTelloManager BetterTelloManager;
+
+    Color customColor = new Color(0f, 0f, 0f, 0.3f);
 
     private void Start()
     {
@@ -23,10 +30,19 @@ public class TogglePause : MonoBehaviour
         TakeOffText= TakeOff.GetComponentInChildren<TMP_Text>();
         ReturnText = Return.GetComponentInChildren<TMP_Text>();
         PatternText = Pattern.GetComponentInChildren<TMP_Text>();
+        ClearText = Clear.GetComponentInChildren<TMP_Text>();
+
     }
 
 
     private void FixedUpdate()
+    {
+
+        updateButtons();
+     
+    }
+
+    private void updateButtons()
     {
         if (BetterTelloManager.ConnectionState == TelloConnectionState.Connected)
         {
@@ -35,32 +51,43 @@ public class TogglePause : MonoBehaviour
             if (BetterTelloManager.FlyingState == BetterTelloLib.Commander.FlyingState.Flying)
             {
                 TakeOffText.text = "Land";
-                Return.interactable= true;
+                Return.interactable = true;
                 ReturnText.color = Color.black;
-                Pattern.interactable= true;
+                Pattern.interactable = true;
                 PatternText.color = Color.black;
             }
             else
             {
                 TakeOffText.text = "TakeOff";
                 Return.interactable = false;
-                ReturnText.color = Color.grey;
-               Pattern.interactable = false;
-                PatternText.color = Color.grey;
+                ReturnText.color = customColor;
             }
         }
-        else { 
-           TakeOff.interactable=false;
-            TakeOffText.color = Color.grey;
+        else
+        {
+            TakeOff.interactable = false;
+            TakeOffText.color = customColor;
             Return.interactable = false;
-            ReturnText.color = Color.grey;
-            Pattern.interactable=false;
-            PatternText.color= Color.grey;
+            ReturnText.color = customColor;
+            Pattern.interactable = false;
+            PatternText.color = customColor;
+        }
+
+        // if there are no targets(i.e. no path), you can't reset path
+        if (BetterTelloManager.Targets.Count == 0)
+        {
+            Pattern.interactable = false;
+            PatternText.color = customColor;
+        }
+        else
+        {
+            Pattern.interactable = true;
+            PatternText.color = Color.black;
         }
     }
 
 
-    private async void UpdateState()
+    private async void UpdateFlightState()
     {
         if (BetterTelloManager.ConnectionState == TelloConnectionState.Connected)
         {
@@ -80,9 +107,17 @@ public class TogglePause : MonoBehaviour
         }
     }
 
+
+
+
     public void OnClick()
     {
-        UpdateState();
+        UpdateFlightState();
+    }
+
+    public void OnClearClick()
+    {
+        //call the clear target function
     }
 
 }
